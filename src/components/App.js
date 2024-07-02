02d6ccab846051c1f59de9171c5b896b80160a64
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-07-02 16:16:04 +0530

    submit 2nd method with 2 states

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,38 @@
-
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
 
 const App = () => {
-  
-  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
-  const [input, setInput] = useState("");
 
-  const productComp = () => { 
-    const SearchFruits = fruits.filter((item) =>
-      item.toLocaleLowerCase().includes(input.toLocaleLowerCase()))
-      .map((item) => { 
-        return <li>{item}</li>;
-      })
-      return SearchFruits
-  }
+  const [input, setInput] = useState("");
+  const [product, setProduct] = useState(fruits);
+
+  useEffect(() => {
+    setProduct(fruits.filter((item) => {
+      return item.toLowerCase().includes(input.toLowerCase())
+    }))
+  }, [input])
 
   return (
     <div>
       <h1>Search item</h1>
-      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
       <ul>
-        <li>{productComp()}</li>
+        {
+        product.map((item) => {
+          return <li>{item}</li>
+        })
+      }
       </ul>
     </div>
   )
 }
 
 export default App
+
+
+
+
+
+
