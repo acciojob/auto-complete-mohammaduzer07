492348ccea108b0311aef7f5f436c70b50ea5e17
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-07-02 16:08:26 +0530

    submit method 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
-import React from "react";
+
+import React, { useState } from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+  
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+  const [input, setInput] = useState("");
+
+  const productComp = () => { 
+    const SearchFruits = fruits.filter((item) =>
+      item.toLocaleLowerCase().includes(input.toLocaleLowerCase()))
+      .map((item) => { 
+        return <li>{item}</li>;
+      })
+      return SearchFruits
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <ul>
+        <li>{productComp()}</li>
+      </ul>
     </div>
   )
 }
